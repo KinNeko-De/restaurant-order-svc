c56560d6f2bdbfefaa14245d8999a30a72b5ab49
Nils Diekmann <nils@diekmann-germany.de>
2022-09-25 12:50:43 +0200

    - better example for breaking changes;

--- Restaurant.SvcOrder/Domain/Orders/SourceEvents/OrderCreated.cs
+++ Restaurant.SvcOrder/Domain/Orders/SourceEvents/OrderCreated.cs
@@ -4,6 +4,12 @@ using Restaurant.SvcOrder.Repositories.Orders.SourceEvents;
 
 namespace Restaurant.SvcOrder.Domain.Orders.SourceEvents;
 
+/// <summary>
+/// Domain source event because:
+/// 1. data types: There is no difference between a Guid and a Guid? in protobuf. also there is no difference between dateonly and datetime
+/// 2. test data based on c# types
+/// 3. common event for breaking changes. alternatively you can always use the highest protobuf source event and convert all other to that
+/// </summary>
 public class OrderCreated : ISourceEvent
 {
     public SourceEventId Id { get; init; }
