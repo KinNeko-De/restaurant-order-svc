698296d6453e9c446e74a13ed64a2e0bcde36f0e
Nils Diekmann <nils@diekmann-germany.de>
2023-01-28 14:18:07 +0100

    - add failing script

---
+++ database/patch/V20220925.1433.56383__willfail.sql
@@ -0,0 +1,8 @@
+CREATE TABLE willfail
+(
+    id uuid NOT NULL
+    sequence_number int NOT NULL
+    record_created_at timestamptz NOT NULL
+    record_updated_at timestamptz
+    PRIMARY KEY (id)
+);
